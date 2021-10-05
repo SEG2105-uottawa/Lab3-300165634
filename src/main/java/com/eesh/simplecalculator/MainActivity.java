252b72f0b4532ef7ada3cccf96c0be24a5371501
Eesh23 <90652129+Eesh23@users.noreply.github.com>
2021-10-04 23:40:55 -0400

    Add files via upload

---
+++ src/main/java/com/eesh/simplecalculator/MainActivity.java
@@ -0,0 +1,91 @@
+package com.eesh.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+
+//Both Code and Android Studio became corrupted and this was a last minute attempt to get some marks. Thank you for grading my work.
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.input);
+    }
+
+    public void btnZero(View view){
+
+    }
+
+    public void btnOne(View view){
+
+    }
+
+    public void btnTwo(View view){
+
+    }
+
+    public void btnThree(View view){
+
+    }
+
+    public void btnFour(View view){
+
+    }
+
+    public void btnFive(View view){
+
+    }
+
+    public void btnSix(View view){
+
+    }
+
+    public void btnSeven(View view){
+
+    }
+
+    public void btnEight(View view){
+
+    }
+
+    public void btnNine(View view){
+
+    }
+
+    public void btnPus(View view){
+
+    }
+
+    public void btnSub(View view){
+
+    }
+
+    public void btnDiv(View view){
+
+    }
+
+    public void btnMul(View view){
+
+    }
+
+    public void btnDec(View view){
+
+    }
+
+    public void btnCle(View view){
+
+    }
+
+    public void btnEqua(View view){
+
+    }
+}
